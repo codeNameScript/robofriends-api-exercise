1675e957adec963f4d6b14ef8707b5f054d50cca
codeNameScript <130048913+codeNameScript@users.noreply.github.com>
2023-04-18 17:03:57 +0900

    removed unnecessary if. The if statement in CardList is useless now because filter has been fixed.

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,62 +2,64 @@ import React, { Component } from 'react';
 import Card from './Card.js';
 
 class CardList extends Component {
+    // props  ['ayaka', 'otherNames',...]
     constructor(props) {
         super(props)
         this.state = {
+            // this is where the json object will go [{name:'charName', other:'data', ...}, ...]
             characterData: []
         }
     }
 
-    componentDidMount() {
+    fetchCharacterData() {
         const { characters } = this.props;
+        // wait for all request to resolve before setState
         Promise.all(
             characters.map(characterName =>
+                // fetching character data
                 fetch(`https://api.genshin.dev/characters/${characterName}`)
                     .then(resp => resp.json())
                     .catch(err => console.log('Oh no', err))
             )
-        ).then(characterDatatmp => {
-            this.setState({ characterData: characterDatatmp });
+        ).then(respCharacterData => {
+            this.setState({ characterData: respCharacterData });
         });
     }
 
+    componentDidMount() {
+        this.fetchCharacterData();
+    }
+
+    // https://legacy.reactjs.org/docs/react-component.html#componentdidupdate
     componentDidUpdate(prevProps) {
+        // only run when prop changed to prevent infinite loop
         if (prevProps.characters !== this.props.characters) {
-            const { characters } = this.props;
-            Promise.all(
-                characters.map(characterName =>
-                    fetch(`https://api.genshin.dev/characters/${characterName}`)
-                        .then(resp => resp.json())
-                        .catch(err => console.log('Oh no', err))
-                )
-            ).then(characterDatatmp => {
-                this.setState({ characterData: characterDatatmp });
-            })
+            this.fetchCharacterData();
         }
     }
 
     render() {
+        // props ['ayaka', 'otherNames',...]
         const { characters } = this.props;
         return (
             <div>
                 {
-                    this.state.characterData.map((character, i) => {
-                        if (characters.includes(characters[i])) {
-                            // you have to return manually if you use {} notation in map dummy
-                            return (
-                                <Card
-                                    key={i}
-                                    id={character.name}
-                                    name={character.name}
-                                    nation={character.nation}
-                                    vision={character.vision}
-                                    weapon={character.weapon}
-                                    // this API does not store images in the object. We have to get them seperately
-                                    imgsrc={`https://api.genshin.dev/characters/${characters[i]}/card`}
-                                />
-                            );
-                        }
+                    this.state.characterData.map((character, i) => {  // character {name:'charName', other:'data', ...}
+                        return (
+                            <Card
+                                key={i}
+                                id={character.name}
+                                name={character.name}
+                                nation={character.nation}
+                                vision={character.vision}
+                                weapon={character.weapon}
+                                // this API does not store images in the json object. We have to get them seperately
+                                imgsrc={`https://api.genshin.dev/characters/${characters[i]}/card`}
+                            />
+                        );
+                        // if (characters.includes(characters[i])) {
+                        //     // you have to return manually if you use {} notation in map dummy
+                        // }
                     })
                 }
             </div>
